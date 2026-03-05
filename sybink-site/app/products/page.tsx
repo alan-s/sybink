a2bd6a13845777b1cce59228b9dde9eb9e65c475
Alan <alan-s@users.noreply.github.com>
2026-03-05 17:07:55 +0000

    Test - products landing page

---
+++ sybink-site/app/products/page.tsx
@@ -0,0 +1,24 @@
+import Banner from '@/app/components/Banner/Banner_Syspos';
+import Companies from '@/app/components/Companies/Companies_Syspos';
+import Buyers from '@/app/components/Buyers/index_syspos';
+import Why from '@/app/components/Why/index_syspos';
+import Newsletter from '@/app/components/Newsletter/Newsletter';
+import Provide from '@/app/components/Provide/index_syspos';
+
+export const metadata = {
+  title: "products",
+  description: "landing page",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+      <Buyers />
+      <Companies />
+      <Why />
+      <Provide />
+      <Newsletter />
+    </main>
+  )
+}
