426058729080c5741dd3d28b0df4270c98c9548e
Alan <alan-s@users.noreply.github.com>
2026-03-12 06:20:23 +0000

    Add OpenGraph tags

--- sybink-site/app/components/Clientsay/index.tsx
+++ sybink-site/app/components/Clientsay/index.tsx
@@ -14,7 +14,7 @@ const Clientsay = () => {
                     <span className="lg:absolute lg:bottom-40 lg:left-80">
                         <Image src={getAssetPath('/assets/clientsay/user.webp')} alt="user-image" width={168} height={168} className="mx-auto pt-10 lg:pb-10" />
                         <div className="lg:inline-block bg-white rounded-2xl p-5 shadow-sm">
-                            <p className="text-base font-normal text-center text-darkgray">Sentinel Membership Management has streamlined our gym's <br />operations. It has made looking after the members <br /> easier and for it we’ve seen increased sales.</p>
+                            <p className="text-base font-normal text-center text-darkgray">Sentinel Membership Management has streamlined our gym's <br />operations. It's made managing the members <br /> effortless and everyone loves it!</p>
                             <h3 className="text-2xl font-medium text-center py-2">Ken Clarke</h3>
                             <h4 className="text-sm font-normal text-center">Muscle Base Gym</h4>
                         </div>
--- sybink-site/app/layout.tsx
+++ sybink-site/app/layout.tsx
@@ -1,11 +1,26 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
+import { getAssetPath } from '@/app/lib/config';
 
 export const metadata = {
-  title: 'Sybink Systems',
-  description: 'Market-driven systems. Built on proven technology.',
-}
+  title: "Sybink Systems | Market-driven systems. Built on proven technology.",
+  description: "Market-driven systems. Built on proven technology.",
+  openGraph: {
+    title: 'Sybink Systems',
+    description: 'Market-driven systems. Built on proven technology.',
+    url: "https://sybink.com",
+    type: "website",
+    images: [
+      {
+        url: getAssetPath("/assets/logo/logo.png"),
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
 
 export default function RootLayout({
   children,
